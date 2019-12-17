091801f6def5cbd3edcde0f26d8633204e030693
Harbdollar <obouncetech@gmail.com>
2019-12-18 00:52:09 +0100

    fifth commit

---
+++ lib/model/staffDetailsClicked.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/foundation.dart';
+
+class StaffDetailsClicked with ChangeNotifier{
+  int _indexClicked = 0;
+
+int get getindexClicked => _indexClicked;
+
+set setIndexClicked(int index){
+  _indexClicked = index;
+ notifyListeners();
+}
+
+
+}
